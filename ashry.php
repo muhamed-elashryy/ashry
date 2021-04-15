69dd14ba93735849af729542c6f762de43fe0abf
Muhamed <muhamedelashryy@gmail.com>
2021-04-15 05:39:38 +0200

    update

---
+++ ashry.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>Home Page</title>
+
+
+    <link class="icon" rel="icon" href="pictures/logo.png" />
+    <link class="icon" rel="shortcut" href="pictures/logo.png" />
+    <link class="icon" rel="apple-touch-icon" href="pictures/logo.png" />
+
+</head>
+
+<body>
+
+    <div class="container">
+        <header>
+            <div id="title">
+                <div class="logo">
+                    <h2><a href="ashry.php">
+                            <i class="fas fa-award"></i>
+
+                            El-ASHRY</a></h2>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="ashry.php">
+                                <i class="fa fa-home"></i>
+                                Home</a></li>
+
+                        <li><a href="https://www.behance.net/muhamedelashry">
+                                <i class='fas fa-dice-d6'></i>
+                                Projects</a></li>
+                        <li><a href="tel:00201120862806">
+                                <i class="fa fa-phone"></i>
+                                Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <div id="name">
+            <h2 class="myname">Muhamed El-Ashry</h2>
+
+        </div>
+        <div id="bar">
+            <h2 class="mybar">Web Developer</h2>
+
+        </div>
+        <div class="mypic">
+
+            <a>
+
+                <img src="pictures/MAN_5345-020.jpeg" style="widows: 138px;height:290px">
+
+                </img>
+            </a>
+
+
+
+
+
+
+
+            <p>
+                Expert in programing and IT for over two years now
+                ( HTML - CSS - JavaScript - PHP - MySql - WordPress ) ..
+                I am very talented in getting your job done
+                in no time with an affordable price that
+                ensures you a high-quality piece of work,
+                we learned that it's all about satisfying
+                the customer !!
+
+
+
+            </p>
+
+
+
+        </div>
+
+        <section class="media">
+
+            <div id="social2">
+                <div class="so2">
+                    <a href="https://www.facebook.com/muhamedelashryy/"><i class='fab fa-facebook-square'></i></a>
+                    <a href="https://twitter.com/muhamedelashryy"> <i class='fab fa-twitter-square'></i></a>
+                    <a href="https://www.linkedin.com/in/muhamed-abd-el-rheem-4716381a4/"><i
+                            class='fab fa-linkedin'></i></a>
+                    <a href="https://www.behance.net/muhamedelashry"><i class='fab fa-behance'></i></a>
+                </div>
+            </div>
+
+        </section>
+
+
+
+
+
+    </div>
+
+
+
+
+    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
